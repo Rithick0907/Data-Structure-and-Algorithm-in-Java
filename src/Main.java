b51fe7b15747443164082a681e4b7c05a1c751c2
RithickB <rithick0907@gmail.com>
2020-12-23 18:16:58 +0530

    Stack implementation added

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,5 @@ import Part1LinearDataStructure.*;     //import all class from Part1OrderedDataS
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.addLast(40);
-        list.addLast(50);
-        list.removeFirst();
-        list.addLast(60);
-        list.addLast(70);
-        list.removeLast();
-        list.addFirst(10);
-        list.print();
     }
 }
---
+++ src/Part1LinearDataStructure/Stack.java
@@ -0,0 +1,49 @@
+package Part1LinearDataStructure;
+
+import java.util.Arrays;
+
+public class Stack {
+    int[] stack;
+    int size;
+    int top;
+
+    public Stack(int capacity){
+        stack = new int[capacity];
+        this.size = capacity;
+    }
+
+    private void resize(){
+        this.size = size * 2;
+        int[] newArray = new int[size];
+        for(int i=0;i<top;i++){
+            newArray[i]=stack[i];
+        }
+        stack=newArray;
+    }
+
+    public void push(int value){
+        if(top>=size){
+            resize();
+        }
+        stack[top++] = value;
+    }
+
+    public int pop(){
+        if(isEmpty())   {
+            return Integer.MIN_VALUE;
+        }
+        return stack[--top];
+    }
+
+    public int peek(){
+        return top==0 ? Integer.MIN_VALUE : stack[top-1];
+    }
+
+    public boolean isEmpty(){
+        return top == 0;
+    }
+
+    public void print(){
+        System.out.println(Arrays.toString(Arrays.copyOfRange(stack,0,top)));
+    }
+}
