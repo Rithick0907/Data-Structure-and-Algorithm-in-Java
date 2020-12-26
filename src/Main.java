ca04554f15864ea757248e243e29c2545d259c2c
RithickB <rithick0907@gmail.com>
2020-12-26 14:26:22 +0530

    Queue implementation added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ import Part1LinearDataStructure.*;     //import all class from Part1OrderedDataS
 
 public class Main {
     public static void main(String[] args) {
+        Queue queue = new Queue(5);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.enqueue(60);
+        queue.enqueue(70);
+
+        System.out.println(queue.toString());
     }
 }
