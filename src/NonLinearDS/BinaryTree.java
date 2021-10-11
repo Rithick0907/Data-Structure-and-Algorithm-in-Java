8246e8ec90258a73374cd21ba45b0acbb498be62
Rithick <rithick0907@gmail.com>
2021-10-11 18:54:14 +0530

    tree height calculation added

--- src/NonLinearDS/BinaryTree.java
+++ src/NonLinearDS/BinaryTree.java
@@ -76,4 +76,15 @@ public class BinaryTree {
         postorder(root.right);
         System.out.println(root.data);
     }
+
+    public int height(){
+        return height(this.root);
+    }
+
+    private int height(Node root){
+        if(root == null)    return -1;
+        if(root.left == null && root.right == null)     return 0;
+
+        return 1 + Math.max(height(root.left),height(root.right));
+    }
 }
