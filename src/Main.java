c05d33e57491423522ef5775c9481410a1193df3
Rithick <rithick0907@gmail.com>
2021-10-14 17:01:45 +0530

    Insertion Sort Implemented

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import NonLinearDS.BinaryTree;
 import SortingAndSearching.BubbleSort;
+import SortingAndSearching.InsertionSort;
 import SortingAndSearching.SelectionSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        SelectionSort ss = new SelectionSort();
+        InsertionSort is = new InsertionSort();
         int[] arr = {7,10,3,0,1,2,3,3,16};
-        ss.sort(arr);
+        is.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
