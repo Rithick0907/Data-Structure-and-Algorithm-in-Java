9c07c0b6b227e5047cec9297809a866032335595
Rithick <rithick0907@gmail.com>
2021-10-16 07:29:07 +0530

    Linear Search Implemented

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import SortingAndSearching.BinarySearch;
+import SortingAndSearching.LinearSearch;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BinarySearch bs = new BinarySearch();
+        var bs = new LinearSearch();
         int[] arr = {7,10,3,0,1,2,3,3,16};
-        Arrays.sort(arr);
-        System.out.println(bs.search(arr,15));
+        System.out.println(bs.search(arr,3));
     }
 }
---
+++ src/SortingAndSearching/LinearSearch.java
@@ -0,0 +1,16 @@
+package SortingAndSearching;
+
+public class LinearSearch {
+    public int search(int[] arr,int target){
+        int result = -1;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
